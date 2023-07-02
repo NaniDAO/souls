1c018f2d183fdd6ba7213f32a3f6f45d299c90ad
Shivanshi Tyagi <shivanshityagi@icloud.com>
2023-07-02 17:01:48 +0530

    test

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.15;
 
 import {Script} from "forge-std/Script.sol";
-
-import {Greeter} from "src/Greeter.sol";
+import {Souls} from "src/Souls.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
     /// @notice The main script entrypoint
-    /// @return greeter The deployed contract
-    function run() external returns (Greeter greeter) {
+    /// @return souls The deployed contract
+    function run() external returns (Souls souls) {
         vm.startBroadcast();
-        greeter = new Greeter("GM");
+        souls = new Souls();
         vm.stopBroadcast();
     }
 }
--- test/Soul.t.sol
+++ test/Soul.t.sol
@@ -2,56 +2,51 @@
 pragma solidity ^0.8.15;
 
 import "@std/Test.sol";
-import {Souls} from "src/Souls.sol";
+import {Souls,TokenStandard} from "src/Souls.sol";
 
-import {ERC721} from "@base/tokens/ERC721/ERC721.sol";
-import {ERC1155} from "@base/tokens/ERC1155/ERC1155.sol";
+import {MockERC721} from "@solmate/test/utils/mocks/MockERC721.sol";
+import {MockERC1155} from "@solmate/test/utils/mocks/MockERC1155.sol";
 
 contract SoulsTest is Test {
     using stdStorage for StdStorage;
 
+    event Soul(address indexed _address, uint256 indexed _tokenId, string _data);
+
     Souls souls;
-    ERC721 erc721;
-    ERC1155 erc1155;
+    MockERC721 erc721;
+    MockERC1155 erc1155;
+    
+    address  alice = makeAddr("alice");
 
     function setUp() external {
         souls = new Souls();
-        erc721 = ERC721(address(this)); // Mock ERC721 contract
-        erc1155 = ERC1155(address(this)); // Mock ERC1155 contract
+        erc721 = new MockERC721("Soul", "SOUL"); // Mock ERC721 contract
+        erc1155 = new MockERC1155(); // Mock ERC1155 contract
     }
 
-    function testSetSoulERC721() external {
-        uint256 tokenId = 1;
-        string memory soulData = "Soul Data";
-
-        // Assume the caller owns the ERC721 token
-        erc721._mint(address(this), tokenId);
+    function testSetSoulERC721(uint256 tokenId, string memory soulData) external {
+        erc721.safeMint(alice, tokenId, "");
 
         // Set the soul of the token
-        souls.set(address(erc721), tokenId, soulData, 721);
-
-        // Expect the Soul event to be emitted
         vm.expectEmit(true, true, true, true);
-        emit souls.Soul(address(erc721), tokenId, soulData);
+        emit Soul(address(erc721), tokenId, soulData);
+        vm.prank(alice);
+        souls.set(address(erc721), tokenId, soulData, TokenStandard.ERC721);
 
         // Check the soul data
         string memory result = souls.metadata(address(erc721), tokenId);
         assertEq(result, soulData);
     }
 
-    function testSetSoulERC1155() external {
-        uint256 tokenId = 1;
-        string memory soulData = "Soul Data";
-
+    function testSetSoulERC1155(uint256 tokenId, string memory soulData) external {
         // Assume the caller owns the ERC1155 token
-        erc1155.mint(address(this), tokenId, 1);
-
+        erc1155.mint(alice, tokenId, 1, "");
         // Set the soul of the token
-        souls.set(address(erc1155), tokenId, soulData, 1155);
-
-        // Expect the Soul event to be emitted
+        
         vm.expectEmit(true, true, true, true);
-        emit souls.Soul(address(erc1155), tokenId, soulData);
+        emit Soul(address(erc1155), tokenId, soulData);
+        vm.prank(alice);
+        souls.set(address(erc1155), tokenId, soulData, TokenStandard.ERC1155);
 
         // Check the soul data
         string memory result = souls.metadata(address(erc1155), tokenId);
